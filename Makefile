a76e79aba27f463eb7aead34dc88888f956861e3
Stephen <sgthomas@ucsc.edu>
2017-07-07 21:05:59 -0700

    Added initial board.h file and main.cpp files

---
+++ Makefile
@@ -0,0 +1,4 @@
+hellomake: main.cpp
+	g++ -o hellomake main.cpp
+clean:
+	rm hellomake
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdexcept>
+#include <unistd.h>
+
+using namespace std;
+
+int main (int argc, char** argv) {
+	cout << "Hello World" << endl;
+}
